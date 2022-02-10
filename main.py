696f074dc04b5d82e740f79864fa9fdd8913e09f
Hassan Sesay Simon <93295035+Koncept55@users.noreply.github.com>
2022-02-10 04:07:02 +0000

    value added to the code

--- main.py
+++ main.py
@@ -5,4 +5,12 @@ print(20 * 24 * 60)
 print(f"20 days are {20 * 24 * 60} minutes")
 print(f"35 days are {35 * 24 * 60} minutes")
 print(f"110 days are {110 * 24 * 60} minutes")
-print(f"20 days are {20 * 24 * 60} minutes")
+
+#string with concantenation
+#Naming convention: convention for naming things
+calculation_to_seconds=24 * 60 * 60
+name_of_units = "seconds"
+print(f"20 days are {20 * calculation_to_seconds} seconds")
+name_of_unit = "hours"
+def days_to_units():
+  print(f"20 days are {20 * calculation_to_seconds} {name_of_units}")
